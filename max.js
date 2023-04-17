1ad6bbb2daf13cdd8c7d9291c59da37090708126
jeongseongcheol <tjdcjf9333@gmail.com>
2023-04-17 22:24:43 +0900

    최댓값

--- max.js
+++ max.js
@@ -1,11 +1,11 @@
-function examFour(array) {
-  //배열만을 인자로 받아서 실행할 수 있도록 작성하시오.
+function examFive(array) {
+  //배열(array)만을 인자로 받아서 실행할 수 있도록 작성하시오.
   if (Array.isArray(array)) {
     if (array.length === 0) {
       console.log("배열안이 비었음");
     } else {
       const minValue = array.reduce((count, cur) => {
-        return Math.min(count, cur);
+        return Math.max(count, cur);
       }, 0);
       return minValue;
     }
@@ -13,4 +13,4 @@ function examFour(array) {
     console.log("배열이 아닙니다.");
   }
 }
-console.log(examFour([3, 1, 4, 2])); //Question 4 최솟값 판별하기
+console.log(examFive([1, 2, 3]));
