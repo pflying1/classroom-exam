2053e89d04661b48a8e94b8ca37324510464d2fa
jeongseongcheol <tjdcjf9333@gmail.com>
2023-05-15 21:22:07 +0900

    자바스크립트를 파이썬으로

--- exam14-python/python.py
+++ exam14-python/python.py
@@ -1,5 +1,5 @@
-function exampleOne(a) {
-  if (typeof a === 'number');{
+""" function exampleOne(a) {
+  if (typeof a === 'number'){
     if(a - Math.floor(a) !== 0) {
       throw new Error('정수를 입력해야 합니다.');
     }
@@ -7,4 +7,15 @@ function exampleOne(a) {
   throw new Error('정수를 입력해야 합니다.')
 }
 return a;
-}
+} """
+
+def exampleOne(a):
+  if type(a) == int:
+    if a - int(a) != 0 :
+      raise ValueError('정수를 입력해야 합니다.')
+  else:
+    raise ValueError('정수를 입력해야 합니다.')
+  return a 
+
+print(exampleOne(3))
+#print(exampleOne(3.2))
