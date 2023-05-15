855c4fb2a45f6958ab442fd87041d0596513f391
jeongseongcheol <tjdcjf9333@gmail.com>
2023-05-15 21:07:55 +0900

    타입스크립트 테스트

--- exam14-python/python.py
+++ exam14-python/python.py
@@ -1,4 +1,4 @@
-function exampleOne(a) {
+""" function exampleOne(a) {
   if (typeof a === 'number');{
     if(a - Math.floor(a) !== 0) {
       throw new Error('정수를 입력해야 합니다.');
@@ -7,4 +7,5 @@ function exampleOne(a) {
   throw new Error('정수를 입력해야 합니다.')
 }
 return a;
-}
+} """
+
